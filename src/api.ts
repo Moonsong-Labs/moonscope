49098dc57435a0cdcf3772bf0a90d52fa2cd0f8a
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-09-19 11:02:34 +0100

    added bearer tokens

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 import { Elysia } from "elysia";
 import { TestDataModel } from "./model";
 import { z } from "zod";
+import { bearer } from "@elysiajs/bearer";
 import { TestDataSchema } from "./types";
 import { Tables } from "./database";
 
@@ -11,6 +12,7 @@ export async function backEndApi() {
   }
 
   return new Elysia()
+    .use(bearer())
     .get("/", () => "Hello World")
     .get("/fetch", async ({ set, headers }) => {
       if (!headers.table) {
@@ -32,7 +34,23 @@ export async function backEndApi() {
       set.status = result ? 202 : 404;
       return { result: result ? result : "NOT FOUND" };
     })
-    .post("/insert", async ({ request, headers, body, set }) => {
+    .post("/insert", async ({ bearer, body, request, headers, set }) => {
+      if (!bearer) {
+        set.status = 400;
+        set.headers[
+          "WWW-Authenticate"
+        ] = `Bearer realm='sign', error="invalid_request"`;
+
+        return "Unauthorized";
+      }
+
+      const authFail = validateBearerToken(bearer);
+
+      if (authFail) {
+        set.status = 401; // Unauthorized
+        return "Unauthorized";
+      }
+
       if (!headers.table) {
         set.status = 400;
         return { status: "error", message: "No table name provided" };
@@ -40,7 +58,10 @@ export async function backEndApi() {
 
       try {
         TestDataSchema.parse(body);
-        const response = await TestDataModel.getInstance(headers.table).insert(headers.moonwallenv!, body as any);
+        const response = await TestDataModel.getInstance(headers.table).insert(
+          headers.moonwallenv!,
+          body as any,
+        );
         return { status: "ok", id: response };
       } catch (error) {
         set.status = 400;
@@ -48,7 +69,11 @@ export async function backEndApi() {
         if (error instanceof z.ZodError) {
           console.error("Bad request received from REST API");
           console.error(error);
-          return { status: "error", message: error.message, detail: error.issues };
+          return {
+            status: "error",
+            message: error.message,
+            detail: error.issues,
+          };
         }
 
         return {
@@ -60,4 +85,19 @@ export async function backEndApi() {
       }
     })
     .listen(3001);
+}
+
+// Placeholder until we have a bearer token generaton source external to the API
+function validateBearerToken(
+  token?: string,
+): false | { status: string; message: string } {
+  const envToken = process.env.MOONSCOPE_TOKEN!;
+  if (envToken !== token) {
+    return {
+      status: "error",
+      message: "Invalid bearer token",
+    };
+  } else {
+    return false;
+  }
 }
